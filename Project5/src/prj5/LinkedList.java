00fe369b9ec3c5708b21b883cebf991777a9ef7c
dashiellbuddVT <dbudd@vt.edu>
2023-04-17 01:28:45 -0400

    added a few linked list methods and node methods

--- Project5/src/prj5/LinkedList.java
+++ Project5/src/prj5/LinkedList.java
@@ -1,8 +1,45 @@
 package prj5;
 
-public class LinkedList {
+public class LinkedList<T> 
+{
+    private Node<T> firstNode;
+    private int size;
+    
     public LinkedList()
     {
+        firstNode = null;
+        size = 0;
+    }
+    
+    public boolean isEmpty()
+    {
+        return size <= 0;
+    }
+    
+    public int size()
+    {
+        return size;
+    }
+    
+    private class Node<D>
+    {
+        private D data;
+        private Node<D> next;
         
+        public Node()
+        {
+            data = null;
+            next = null;
+        }
+        
+        public Node<D> getNext()
+        {
+            return next;
+        }
+        
+        public D getData()
+        {
+            return data;
+        }
     }
 }
